92cd8a9df96a1e087c92c8d88ed25f8e9f3fea44
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-11 22:18:57 +0500

    Add star pattern generation function: implement printStars to display decreasing rows

---
+++ Patterns/star-pattern-5.js
@@ -0,0 +1,12 @@
+function printStars(n) {
+
+    for (let i = n; i >=1; i--) {
+        let row = "";
+        for (let j = 1; j <= i; j++) {
+            row += "*";
+        }
+        console.log( row);
+    }
+}
+
+printStars(5)
