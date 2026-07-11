6e1cd653c0bfcfe78f8d151d1720f256619bf3b9
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-11 22:22:29 +0500

    Add number pattern generation function: implement printNum to display decreasing rows

---
+++ Patterns/num-pattern-6.js
@@ -0,0 +1,12 @@
+function printNum(n){
+    for (let i = n; i >= 1; i--) {
+        let row = "";
+        for (let j = 1; j <= i; j++) {  
+            row += j
+        }
+        console.log(row);
+        
+    }
+}
+
+printNum(5)
