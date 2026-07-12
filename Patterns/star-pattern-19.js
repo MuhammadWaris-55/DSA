001edbfbd57086bac217c7070b176275f98fea31
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-12 15:55:42 +0500

    Add star pattern generation function: implement print to display a diamond shape of stars

---
+++ Patterns/star-pattern-19.js
@@ -0,0 +1,17 @@
+function printStars(n) {
+    //upper half
+    for (let i = 1; i <= n; i++) {
+        let stars = n - i + 1;
+        let gap = 2 * (i + 1);
+        console.log("*".repeat(stars) + " ".repeat(gap) + "*".repeat(stars))
+    }
+
+    //lover half
+    for (let i = n - 1; i >= 1; i--) {
+        let stars = n - i + 1;
+        let gap = 2 * (i + 1);
+        console.log("*".repeat(stars) + " ".repeat(gap) + "*".repeat(stars))
+    }
+}
+
+printStars(5)
