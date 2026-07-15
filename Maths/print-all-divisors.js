0b225bfe1e34e068f9d82a155679e8aee02d915d
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-07-16 00:28:16 +0500

    Optimize divisor calculation: implement efficient algorithm to find and sort divisors

--- Maths/print-all-divisors.js
+++ Maths/print-all-divisors.js
@@ -6,15 +6,38 @@
 // Output: [1, 2, 3, 4, 6, 12]
 // Explanation: The divisors of 12 are 1, 2, 3, 4, 6, 12.
 
+// function print(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// print(36)
+
+// Time Complexity: O(N)
+
+
+//With better approach
 function print(n) {
-    for (let i = 1; i <= n; i++) {
+    let divisors = []
+
+    for (let i = 1; i * i <= n; i++) {
         if (n % i === 0) {
-            console.log(i)
+            divisors.push(i)
+
+            if (i !== n / i) {
+                divisors.push(n / i)
+            }
         }
     }
+
+    divisors.sort((a, b) => a - b)
+    console.log(divisors)
 }
 
 print(36)
 
 
-// Time Complexity: O(N)
+// Time Complexity: O(√n)
